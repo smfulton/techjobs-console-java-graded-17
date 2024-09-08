1462b24d80569d2f0b7947a8dfb17a75a7115b69
smfulton <sfulton1996@gmail.com>
2024-09-07 19:31:29 -0500

    Assignment complete excluding list sort. Implement before submission.

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -116,7 +116,7 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        if(someJobs.size() == 0){
+        if(someJobs.isEmpty()){
             System.out.println("No Results");
         }
         else{
